1af2171e51d401d28008373add240df247fc36e9
Saravanan10297 <139671613+Saravanan10297@users.noreply.github.com>
2023-08-11 19:51:24 +0530

    first code

---
+++ project2/src/test/java/H2DB/com/project2/Project2ApplicationTests.java
@@ -0,0 +1,14 @@
+package H2DB.com.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
